abe7e98342d85645cccbb152bbc36a5a55ccbc15
June Cong <junzhucong@gmail.com>
2017-01-14 16:26:39 -0800

    huge refactor to change default tip entry and clean up code

--- tipCalculator/SettingsViewController.swift
+++ tipCalculator/SettingsViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    @IBOutlet weak var defaultTip: UITextField!
-    @IBOutlet weak var LabelDefaultTipDisplay: UILabel!
+    @IBOutlet weak var defaultTip1: UITextField!
+    @IBOutlet weak var defaultTip2: UITextField!
+    @IBOutlet weak var defaultTip3: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,19 +26,47 @@ class SettingsViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        defaultTip1.becomeFirstResponder()
         let defaults = UserDefaults.standard
-        LabelDefaultTipDisplay.text = String(defaults.integer(forKey: "defaultKey"))
-        defaultTip.becomeFirstResponder()
+        defaultTip1.text = String(Int(defaults.double(forKey: "tipInt1")*100))
+        defaultTip2.text = String(Int(defaults.double(forKey: "tipInt2")*100))
+        defaultTip3.text = String(Int(defaults.double(forKey: "tipInt3")*100))
+    }
+
+
+    @IBAction func changeOne(_ sender: Any) {
+        print("settings")
+        var value = Double(defaultTip1.text!)
+        value = value!/100
+        syncDefaults(dub: value!, index: 0)
+    }
+    
+    @IBAction func changeTwo(_ sender: Any) {
+        var value = Double(defaultTip2.text!)
+        value = value!/100
+        syncDefaults(dub: value!, index: 1)
     }
     
     
-    @IBAction func onChange(_ sender: Any) {
-        LabelDefaultTipDisplay.text = defaultTip.text
+    @IBAction func changeThree(_ sender: Any) {
+        var value = Double(defaultTip3.text!)
+        value = value!/100
+        syncDefaults(dub: value!, index: 2)
+    }
+
+
+    func syncDefaults(dub: Double, index: Int)  {
+        let arrayOfStrings: [String] = ["tipInt1", "tipInt2", "tipInt3"]
         let defaults = UserDefaults.standard
-        defaults.set(Int(LabelDefaultTipDisplay.text!), forKey: "defaultKey")
+        defaults.set(dub, forKey: arrayOfStrings[index])
         defaults.synchronize()
-    
         
+    }
+
+    
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
     }
     
 
--- tipCalculator/ViewController.swift
+++ tipCalculator/ViewController.swift
@@ -14,13 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipNum: UILabel!
     @IBOutlet weak var totalNum: UILabel!
     @IBOutlet weak var tipChoose: UISegmentedControl!
-
-    @IBOutlet weak var partyOf: UISegmentedControl!
-    @IBOutlet weak var defaultTip: UILabel!
+    var tipPercentages = [0.18, 0.2, 0.22]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,9 +27,11 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         let defaults = UserDefaults.standard
-        defaultTip.text = String(defaults.integer(forKey: "defaultKey"))
-        defaultTip.textColor = UIColor.black
+        let billExisting = defaults.integer(forKey: "bill")
         billNum.becomeFirstResponder()
+        if (billExisting != 0) {
+            billNum.text = String(billExisting)
+        }
 
     }
     
@@ -44,50 +43,45 @@ class ViewController: UIViewController {
 
     
     @IBAction func calculateTip(_ sender: AnyObject) {
-        
-        let bill = Double(billNum.text!) ?? 0
-        
         let defaults = UserDefaults.standard
-        defaultTip.text = String(defaults.integer(forKey: "defaultKey"))
-
-        let tipDub = (Double(defaultTip.text!)!/100)
-        
-        let tip = bill * tipDub
-        let total = bill + tip
-        //partySelected(total: total, tip: tip)
-        
-        tipNum.text = String(format: "$%.2f", tip)
-        totalNum.text = String(format: "$%.2f", total)
+        let bill = Double(billNum.text!) ?? 0
+        defaults.set(bill, forKey: "bill")
+        let tipIndex = tipChoose.selectedSegmentIndex
+        changeTextLabels(tipInt: tipIndex, bill: bill)
         
     }
     
     @IBAction func nonDefaultTip(_ sender: Any) {
-        print( "nonDefaultTipSelected running")
-        let tipPercentages = [0.18, 0.2, 0.22]
         let bill = Double(billNum.text!) ?? 0
-        let tip = bill * tipPercentages[tipChoose.selectedSegmentIndex]
-        let total = bill + tip
-        defaultTip.textColor = UIColor.white
-        //partySelected(total: total, tip: tip)
+        let tipIndex = tipChoose.selectedSegmentIndex
+        changeTextLabels(tipInt: tipIndex, bill: bill)
+    }
+    
+
+    func changeTextLabels(tipInt:Int, bill:Double) {
+        let defaults = UserDefaults.standard
         
+        let tipInt1 = defaults.double(forKey: "tipInt1")
+        let tipInt2 = defaults.double(forKey: "tipInt2")
+        let tipInt3 = defaults.double(forKey: "tipInt3")
+        
+        tipChoose.setTitle(String(tipInt1*100), forSegmentAt: 0)
+        tipChoose.setTitle(String(tipInt2*100), forSegmentAt: 1)
+        tipChoose.setTitle(String(tipInt3*100), forSegmentAt: 2)
+
+        tipPercentages[0] = tipInt1
+        tipPercentages[1] = tipInt2
+        tipPercentages[2] = tipInt3
+        
+        let tip = bill * tipPercentages[tipInt]
+        let total = bill + tip
         tipNum.text = String(format: "$%.2f", tip)
         totalNum.text = String(format: "$%.2f", total)
     }
     
-    /*
-    func partySelected(total:Double, tip:Double) {
-        let selectedParty = partyOf.selectedSegmentIndex
-        //let selectedParty = 2
-        let partySizes = [1, 2, 3]
-        
-        let final = total/Double(partySizes[selectedParty])
-        //print(final)
-        totalNum.text = String(format: "$%.2f", final)
-        tipNum.text = String(format: "$%.2f", tip)
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
     }
-    */
-    
 
 }
 
